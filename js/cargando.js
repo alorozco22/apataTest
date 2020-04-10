02b162aa53fbe753ebb38265cad0076bedb093a2
Alfredo <ae.orozco10@uniandes.edu.co>
2020-04-10 11:04:15 -0500

    imágenes y comentarios sobre imágenes

--- js/cargando.js
+++ js/cargando.js
@@ -27,6 +27,19 @@ let desaparecer = function(){
 
 // que se vayan cargando las montanitas volviendo negra
 // que en movil ocupe toda la pantalla en vertical
-// 5 segundos de carga
+// CHULO 5 segundos de carga
 // necesitamos mas cosas para responsive? del diseniador
 //para miercoles tener esa info del ilustrador - anchos, dos imagenes, uno para cel
+
+//https://stackoverflow.com/questions/14825206/grayscale-on-only-part-of-an-image
+
+/**
+Notas sobre la imagen:
+- Está top! Muy útil incluirlo con guías y sin guías, para mí. Sería chévere si al final fuera un nevado! no? uff.
+- Pregunta! vamos a animar a las personas? Porque si es así, necesitamos que:
+	1. El fondo no tenga personas ni objetos ni nada porque los vamos a poner encima.
+	2. Necesitaríamos al menos dos ilustraciones de cada objeto.. por ejemplo.. si hay alguien caminando, entonces una foto con la pierna derecha adelante y otra con la pierna atrás.. A los árboles se les pueden variar punticos para que parezca que el viento mueve sus hojas.. 
+- La que no tiene guías, le podemos quitar el logo de apata, y los textos.. todos los textos los ponemos con código.
+- Ya con esto me doy cuenta, creo que no necesitamos dos ilustraciones diferentes para movil y compu.. solo que las cosas estén separaditas. Ya con eso, chulo, con código lo modificamos.
+- Uff ojalá creara imágenes de objetos que vuelan.. como parapentes.. o globos.. Depronto mejor parapentes.. o pájaros y Nubes.. Pero como objetos aparte. Recuerde que todo objeto toca mínimo dos variaciones... para hacerlo animado.
+**/
