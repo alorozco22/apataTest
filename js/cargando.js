b4d31d4daf8084c603bfec961e24ec40c5dee9c1
Alfredo <ae.orozco10@uniandes.edu.co>
2020-04-10 10:31:23 -0500

    5 segundos de carga

--- js/cargando.js
+++ js/cargando.js
@@ -1,4 +1,7 @@
 let nohacargado = true;
+let tiempoMinimoYaPaso = false;
+
+setTimeout(function(){tiempoMinimoYaPaso=true;}, 5000);
 
 $(window).on("scroll", function(){
 	if (nohacargado){
@@ -7,6 +10,23 @@ $(window).on("scroll", function(){
 });
 
 $(window).on("load",function(){
-     $(".loader-wrapper").fadeOut("slow");
-     setTimeout(function(){ nohacargado = false; }, 1000);
+     desaparecer();
 });
+
+
+let desaparecer = function(){
+
+	if (!tiempoMinimoYaPaso){
+		setTimeout(function(){desaparecer(),1000});	
+	} else {
+		$(".loader-wrapper").fadeOut("slow");
+     	setTimeout(function(){ nohacargado = false; }, 1000);
+	}
+}
+
+
+// que se vayan cargando las montanitas volviendo negra
+// que en movil ocupe toda la pantalla en vertical
+// 5 segundos de carga
+// necesitamos mas cosas para responsive? del diseniador
+//para miercoles tener esa info del ilustrador - anchos, dos imagenes, uno para cel
